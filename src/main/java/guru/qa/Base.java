e4acbbec01f0e01893cd9c06667b3424d5536a79
Ekaterina_Vasilevskaya <es-vasilevskaya@yandex.ru>
2022-06-01 23:31:05 +0700

    added Base class with data types

---
+++ src/main/java/guru/qa/Base.java
@@ -0,0 +1,42 @@
+package guru.qa;
+
+public class Base {
+
+    boolean isTrue = true;
+    boolean isFalse = false;
+
+    //byte - целое число от -128 до 127
+    byte a = -128;
+    byte b = 127;
+    byte aa = Byte.MIN_VALUE;
+    byte bb = Byte.MAX_VALUE;
+
+    //short - целое число от -32768 до 32767
+    short c = -32768;
+    short d = 32767;
+    short cc = Short.MIN_VALUE
+    short dd = Short.MAX_VALUE;
+
+    //int - целое число от -2147483648 до 2147483647
+    int ee = Integer.MIN_VALUE;
+    int ff = Integer.MAX_VALUE;
+
+    //long - целое число от –9 223 372 036 854 775 808 до 9 223 372 036 854 775 807
+    long gg = Long.MIN_VALUE;
+    long hh = Long.MAX_VALUE;
+
+     //float - число с плавающей точкой от -3.4E+38 до 3.4E+38
+    float ii = Float.MIN_VALUE;
+    float jj = Float.MAX_VALUE;
+
+    //double - число с плавающей точкой от -1.7E+308 до 1.7E+308
+    double kk = Double.MIN_VALUE;
+    double ll = Double.MAX_VALUE;
+
+    //char - одиночный символ
+    char m = 'q';
+
+    //string - строки, НЕ примитивный тип данных
+    String name = "Kate";
+
+}
